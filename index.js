bdf9776cd489585dc27e45e6059dc5be6c6b4bd2
eggsvonsatan <elbadry.d@gmail.com>
2018-07-08 01:30:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // Add your doToElementsInArray() function here:
 function doToElementsInArray(array, callback){
   array.forEach(callback)
-}
+};
 function changeCompletely(array[i], i, callback){
  array[i] = array[i] + "are cool"
-}
+};
 
 // Add your changeCompletely() function here:
