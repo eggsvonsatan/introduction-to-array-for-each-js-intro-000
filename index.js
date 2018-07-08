231ff7a9fc26f921d2be6fffc96c4c8011c28760
eggsvonsatan <elbadry.d@gmail.com>
2018-07-08 01:59:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Add your doToElementsInArray() function here:
 function changeCompletely(element, index, array){
- array[index] = `${array[index]} has balls`;
+ array[index] = array[index].toUpperCase();
 };
 function doToElementsInArray(array, callback){
   array.forEach(callback)
