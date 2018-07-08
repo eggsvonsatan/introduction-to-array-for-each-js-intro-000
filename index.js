5badf8982a757c3bde174798607634dfb9b03541
eggsvonsatan <elbadry.d@gmail.com>
2018-07-08 01:46:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Add your doToElementsInArray() function here:
 function changeCompletely(element, index, array){
- array[index] = `${array[index]}s!!!`;
+ array[index] = `${array[index]} has balls`;
 };
 function doToElementsInArray(array, callback){
   array.forEach(callback)
